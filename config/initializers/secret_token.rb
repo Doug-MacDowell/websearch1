5322c2611f3f5e1cb3bba4acbca27f71235b33a9
Doug MacDowell <dougmac9@uw.edu>
2014-01-14 16:31:14 -0800

    initial setup structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Websearch1::Application.config.secret_key_base = '0bc29edb50d50840749b66c619b333d912260eba284cd9f669923fe432e953302dff62f30260b3709c421bc9322383f75dd18d7ff9a71c58bbcbb6356155da06'
